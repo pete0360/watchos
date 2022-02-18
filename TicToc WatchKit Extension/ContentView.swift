f59db4b30833117fd35f395946d956888dc56c2a
Vladimir Cezar <vladimircezar@icloud.com>
2022-02-18 10:22:00 -0500

    Add UI elements

--- TicToc WatchKit Extension/ContentView.swift
+++ TicToc WatchKit Extension/ContentView.swift
@@ -9,8 +9,26 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
-            .padding()
+      VStack(alignment: .center, spacing: 8) {
+        Text("0")
+          .font(.system(size: 90))
+          .fontWeight(.black)
+          .multilineTextAlignment(.center)
+        HStack(alignment: .center, spacing: 8) {
+          Button {
+            print("Increment")
+          } label: {
+           Image(systemName: "plus")
+              .font(.system(size: 34))
+          }
+          Button {
+            print("Increment")
+          } label: {
+            Text("Go!")
+              .font(.system(size: 34))
+          }
+        }
+      }
     }
 }
 
