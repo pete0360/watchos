42a156721440c01304fb7d015b6b705c65e719e9
Vladimir Cezar <vladimircezar@icloud.com>
2022-02-18 09:59:45 -0500

    Initial Commit

---
+++ TicToc WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TicToc WatchKit Extension
+//
+//  Created by Vladimir Cezar on 2022-02-18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TicToc WatchKit Extension/TicTocApp.swift
@@ -0,0 +1,19 @@
+//
+//  TicTocApp.swift
+//  TicToc WatchKit Extension
+//
+//  Created by Vladimir Cezar on 2022-02-18.
+//
+
+import SwiftUI
+
+@main
+struct TicTocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
