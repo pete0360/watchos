efaad56849827ff6f3574336355932fecb18ac4a
Vladimir Cezar <vladimircezar@icloud.com>
2022-02-24 11:41:03 -0500

    Update counter

--- TicToc WatchKit Extension/ContentView.swift
+++ TicToc WatchKit Extension/ContentView.swift
@@ -10,35 +10,35 @@ import Combine
 
 struct ContentView: View {
   
-    @State private var count = 0
+  @State private var count = 0
   private var timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
   
-    var body: some View {
-      VStack(alignment: .center, spacing: 8) {
-        Text("\(count)")
-          .font(.system(size: 90))
-          .fontWeight(.black)
-          .multilineTextAlignment(.center)
-          .onReceive(timer) { time in
-            print("\(time)")
-          }
-        HStack(alignment: .center, spacing: 8) {
-          Button {
-            print("Increment")
-            count = count + 1
-          } label: {
-           Image(systemName: "plus")
-              .font(.system(size: 34))
-          }
-          Button {
-            print("Increment")
-          } label: {
-            Text("Go!")
-              .font(.system(size: 34))
-          }
+  var body: some View {
+    VStack(alignment: .center, spacing: 8) {
+      Text("\(count)")
+        .font(.system(size: 90))
+        .fontWeight(.black)
+        .multilineTextAlignment(.center)
+        .onReceive(timer) { _ in
+          count = count > 0 ? count - 1 : 0
+        }
+      HStack(alignment: .center, spacing: 8) {
+        Button {
+          print("Increment")
+          count = count + 1
+        } label: {
+         Image(systemName: "plus")
+            .font(.system(size: 34))
+        }
+        Button {
+          print("Increment")
+        } label: {
+          Text("Go!")
+            .font(.system(size: 34))
         }
       }
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
